17d5df563c6492600b3c501747dc25e6e560f4c0
Core <daverencordero@gmail.com>
2023-11-12 20:01:35 +0800

    chore: made tests more appropriate

--- crates/funlang_derive/tests/error_test.rs
+++ crates/funlang_derive/tests/error_test.rs
@@ -2,20 +2,20 @@ use funlang_error::{ErrorCascade, ErrorSpan};
 
 #[derive(funlang_derive::Error)]
 enum ParserError {
-    #[message = "source is missing"]
-    MissingSource,
+    #[message = "something went wrong during addition"]
+    AdditionException,
     #[message = "number is invalid"]
     InvalidNumber,
 }
 
 fn main() {
-    let cascaded_error = ErrorCascade::new(ParserError::InvalidNumber)
+    let cascaded_error = ErrorCascade::new(ParserError::AdditionException)
         .set_span(ErrorSpan::new(0, 1, 1))
-        .set_embedded_error(Box::new(ErrorCascade::new(ParserError::MissingSource)));
-    let cascaded_span_error = ErrorCascade::new(ParserError::InvalidNumber)
+        .set_embedded_error(Box::new(ErrorCascade::new(ParserError::InvalidNumber)));
+    let cascaded_span_error = ErrorCascade::new(ParserError::AdditionException)
         .set_span(ErrorSpan::new(0, 1, 1))
         .set_embedded_error(Box::new(
-            ErrorCascade::new(ParserError::MissingSource).set_span(ErrorSpan::new(0, 2, 1)),
+            ErrorCascade::new(ParserError::InvalidNumber).set_span(ErrorSpan::new(0, 2, 1)),
         ));
 
     println!("cascaded error: {}", cascaded_error);
